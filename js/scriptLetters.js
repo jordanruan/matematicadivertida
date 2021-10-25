cadea93a69599c212fa1d0a4fca77d3cf7efa26e
Rodolpho <rodolpho_almeida@hotmail.com>
2021-10-25 18:27:18 -0300

    Acessibilidade, alteração do tamanho das letras

---
+++ js/scriptLetters.js
@@ -0,0 +1,8 @@
+$('#visionPlus').click(function(){
+	$('*').css('font-size', '105%');
+	$('#cotacao').css('font-size', '300%');
+})
+$('#visionMinus').click(function(){
+	$('*').css('font-size', '100%');
+	$('#cotacao').css('font-size', '300%');
+}) 

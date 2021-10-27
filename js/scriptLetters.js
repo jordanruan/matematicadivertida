1b2f2c806a3e70a6db09be93b322253e6aa09541
Rodolpho <rodolpho_almeida@hotmail.com>
2021-10-27 07:57:10 -0300

    melhorias na responsividade

--- js/scriptLetters.js
+++ js/scriptLetters.js
@@ -1,9 +1,18 @@
-// função para aumentar e diminuir algumas letras precisam ser tratadas separadas.
+// função para aumentar e diminuir alguns items precisaram ser tratados separados.
 $('#visionPlus').click(function(){
 	$('*').css('font-size', '105%');
 	$('#cotacao').css('font-size', '300%');
+	$('.titulo-home').css('font-size', '4rem');
+	$('.titulo-model').css('font-size', '2rem');
+	$('.devcontent h1').css('font-size', '3rem');
+	$('.details h1').css('font-size', '3rem');
+
 })
 $('#visionMinus').click(function(){
 	$('*').css('font-size', '100%');
 	$('#cotacao').css('font-size', '300%');
+	$('.titulo-home').css('font-size', '4rem');
+	$('.titulo-model').css('font-size', '2rem');
+	$('.devcontent h1').css('font-size', '3rem');
+	$('.details h1').css('font-size', '3rem');
 }) 
